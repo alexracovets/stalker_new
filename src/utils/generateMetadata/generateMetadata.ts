830b789886ad828178b63e313de2358d83b8d82f
Oleksandr <alexracovets@gmail.com>
2025-01-30 09:35:57 +0200

    add favicon

--- src/utils/generateMetadata/generateMetadata.ts
+++ src/utils/generateMetadata/generateMetadata.ts
@@ -11,28 +11,22 @@ export const generateMetadata = ({
     title,
     description,
     keywords = ["stalker", "game", "database"],
-    image = "/default-og.jpg",
 }: MetaParams): Metadata => {
     return {
         title: `${title} | Stalker 2`,
         description,
         keywords,
+        icons: {
+            icon: '/favicon.ico',
+        },
         openGraph: {
             title: `${title} | Stalker 2`,
             description,
-            images: [
-                {
-                    url: image,
-                    width: 1200,
-                    height: 630,
-                },
-            ],
         },
         twitter: {
             card: "summary_large_image",
             title: `${title} | Stalker 2`,
             description,
-            images: [image],
         },
     };
 };
