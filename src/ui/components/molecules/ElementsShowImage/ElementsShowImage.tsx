195c7e999be8b94ed7b607a8e901f1112606476e
Oleksandr <alexracovets@gmail.com>
2025-02-14 02:36:36 +0200

    fixed style

--- src/ui/components/molecules/ElementsShowImage/ElementsShowImage.tsx
+++ src/ui/components/molecules/ElementsShowImage/ElementsShowImage.tsx
@@ -13,8 +13,8 @@ export const ElementsShowImage = ({ image, name, variant }: ElementsShowImageTyp
     return (
         <AtomImage
             wrapper={cn(
-                "w-[38.8rem] h-[38.8rem]",
-                variant === "secondary" && "w-[25.6rem] h-[25.6rem]"
+                "w-[38.8rem] h-[38.8rem] mt-[7.4rem]",
+                variant === "secondary" && "w-[25.6rem] h-[25.6rem] mt-[8.4rem]"
             )}
             src={image}
             alt={name}
